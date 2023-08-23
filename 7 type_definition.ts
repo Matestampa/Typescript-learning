ebf5be8f55eca66d66df2487e9c07b46f9836f6c
Matestampa <69252997+Matestampa@users.noreply.github.com>
2023-08-23 20:52:50 -0300

    Add files via upload

---
+++ 7 type_definition.ts
@@ -0,0 +1,20 @@
+//--------------------- Definiciond de tipos (onda el typedef de c++ con huge) --------------
+
+//Podemos definir tipos que pueden usar las variables despues.
+
+//----- Definicion -----
+//Es type "nombre":"tipo o tipos"
+
+type alfanumerico=string|number;
+
+type error_status="Fatla"|"Critical"|"Disaster"|"Omaaaa";
+
+type uuid=`${string}-${string}-${string}`;
+
+/*Midu
+-Readonly y optional props en objects
+
+-Union types y intersection
+
+
+*/
