85aa2ed3389c5690be47a9b05375fbd464d16221
Matestampa <69252997+Matestampa@users.noreply.github.com>
2023-08-23 21:43:47 -0300

    Add files via upload

--- Learning/1 types/7 type_definition.ts
+++ Learning/1 types/7 type_definition.ts
@@ -5,6 +5,35 @@
 //----- Definicion -----
 //Es type "nombre":"tipo o tipos"
 
-type alfanumerico=string|number;
+//Aca creamos un type que acepte string y numeros
+type Alfanumerico=string|number;
 
-type error_status="Fatla"|"Critical"|"Disaster"|"Omaaaa"
+//Aca creamos uno que solo acete ciertos string (podemos hacer lo mismo con numeros)
+type Error_status="Fatal"|"Critical"|"Disaster"|"Omaaaa";
+
+//Tmb se pueden establecer formatos de strings (ej para un uuid)
+type Uuid=`${string}-${string}-${string}`;
+
+//Aca creamos un object type
+type Person={
+    id:Uuid; //usamos uno de los types de arriba
+    name:string,
+    apellido:string,
+    age:number
+};
+
+
+//-------- Implementaciones ------------
+let num:Alfanumerico=9;
+num="nueve";
+
+
+function handle_error(error:Error_status):void{
+    //segun el tipo de error(q solo pueden ser esas opciones) hace algo
+}
+
+
+function create_randomPerson():Person{
+    //crear cosas random
+    return {id:"3123-de33-23d",name:"tute",apellido:"rodr",age:20};
+}
